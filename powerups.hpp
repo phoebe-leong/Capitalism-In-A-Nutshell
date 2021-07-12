e0256d95866a05730672f9a3f62097611ae20e8f
ph3b3 <phoebeleong8@gmail.com>
2021-07-12 15:12:50 +1000

    7.2

--- powerups.hpp
+++ powerups.hpp
@@ -10,6 +10,14 @@ struct Powerups {
     struct Wait {
         int maxCommands = 10;
         int commands = 0;
+
+        void updateDifficultyLevel(std::string dif) {
+            if (dif == "Easy") {
+                maxCommands *= 2;
+            } else if (dif == "Impossible") {
+                maxCommands /= 2;
+            }
+        }
     };
     
         int incomeMultiplier = 200;
@@ -19,4 +27,14 @@ struct Powerups {
         int payrise = 250;
         std::string powerup2 = "Payrise Powerup";
         bool payrisePowerupActive = false;
+
+        void updateDifficultyLevel(std::string dif) {
+            if (dif == "Easy") {
+                incomeMultiplier *= 2;
+                cost -= 500;
+            } else if (dif == "Impossible") {
+                incomeMultiplier /= 20;
+                cost *= 100;
+            }
+        }
 };
